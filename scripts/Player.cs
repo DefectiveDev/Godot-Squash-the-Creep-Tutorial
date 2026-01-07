c727221e43f59a94c3ee27193f9031107579e94b
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-07 18:55:56 -0500

    set up player movement added player to main scene added camera to main scene

---
+++ scripts/Player.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    [Export]
+    public int Speed = 14;
+
+    [Export]
+    public int FallAcceleration = 75;
+
+    private Vector3 _targetVelocity = Vector3.Zero;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var direction = new Vector3(Input.GetAxis("move_left", "move_right"), 0f,
+                                    Input.GetAxis("move_forward", "move_back"))
+                                    .Normalized();
+
+        if (direction != Vector3.Zero)
+        {
+            GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+        }
+
+        _targetVelocity.X = direction.X * Speed;
+        _targetVelocity.Z = direction.Z * Speed;
+
+        if (!IsOnFloor())
+        {
+            _targetVelocity.Y -= FallAcceleration * (float)delta;
+        }
+
+        Velocity = _targetVelocity;
+        MoveAndSlide();
+    }
+}
