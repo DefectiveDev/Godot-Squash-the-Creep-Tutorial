4582accc9900049942f6e682d696cc605ce46f12
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-07 20:11:44 -0500

    set physics layers for world, player, and enemies

---
+++ scripts/Main.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+    [Export]
+    public PackedScene MobScene;
+
+    private void OnMobTimerTimeout()
+    {
+        Mob mob = MobScene.Instantiate<Mob>();
+
+        var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+
+        mobSpawnLocation.ProgressRatio = GD.Randf();
+
+        Vector3 playerPosition = GetNode<Player>("Player").Position;
+
+        mob.Initialize(mobSpawnLocation.Position, playerPosition);
+
+        AddChild(mob);
+    }
+}
